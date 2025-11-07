e790cd81da76aaa2240e9fd1eba3cf526be18c51
ZhiyuShen71 <zhiyu.shen@student.uva.nl>
2025-11-07 16:00:56 +0000

    Finished tasks 1-3

--- tasks.py
+++ tasks.py
@@ -19,7 +19,14 @@ from sklearn import datasets
 # -----------------------------------------------
 def my_mlp(w, X, sigma=np.tanh):
 
-    return f
+  W1 = np.array(w[0:4*6]).reshape(4,6)
+  W2 = np.array(w[4*6:7*4+6*4]).reshape(7,4)
+  W3 = np.array(w[7*4+6*4:]).reshape(1,7)
+   
+  a1 = sigma(W1 @ X)
+  a2 = sigma(W2 @ a1)
+  f  = sigma(W3 @ a2)
+  return f
 # -----------------------------------------------
  
 # Task 2:
@@ -33,7 +40,9 @@ def my_mlp(w, X, sigma=np.tanh):
 # Copy and paste the code for that function here:
 # -----------------------------------------------
 def MSE_func(w, X, y): # give the appropriate name and arguments
-
+    f = my_mlp(w, X)
+    MSE = np.sum((y-f)**2)
+    return MSE
     return MSE
 # -----------------------------------------------
  
@@ -51,7 +60,9 @@ def MSE_func(w, X, y): # give the appropriate name and arguments
 # -----------------------------------------------
 def dR(beta, x, y):
     # implement the above formula for dR/dβ₀
+    dbeta_0 = 2*np.mean((beta[0] + beta[1]*x - y)) 
     # implement the above formula for dR/dβ₁
+    dbeta_1 = 2*np.mean((beta[0] + beta[1]*x - y)*x)
     return np.array([dbeta_0, dbeta_1])
  
 # -----------------------------------------------
